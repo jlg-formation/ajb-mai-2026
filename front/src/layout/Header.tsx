88e4b2a8eb02bc8839be88e4fe47fd5b6aad1789
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-05-27 10:35:32 +0200

    feat: add layout

---
+++ front/src/layout/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="#">
+        <img src="/logo.svg" alt="Gestion Stock Logo" />
+        <span>Gestion Stock</span>
+      </a>
+    </header>
+  );
+}
