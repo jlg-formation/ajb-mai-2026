98c4dc358e680974e91375166de74e723dbaf246
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-05-28 15:07:06 +0200

    feat: integrate @tanstack/react-form for article addition with validation

--- front/src/routes/stock/add.tsx
+++ front/src/routes/stock/add.tsx
@@ -1,30 +1,95 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { createFileRoute } from "@tanstack/react-router";
+import { useForm } from "@tanstack/react-form";
+import type { NewArticle } from "../../types/Article";
 
 export const Route = createFileRoute("/stock/add")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const form = useForm({
+    defaultValues: {
+      name: "Truc",
+      price: 0,
+      qty: 1,
+    } satisfies NewArticle,
+    onSubmit: async ({ value }) => {
+      console.log(value);
+
+      await new Promise((r) => setTimeout(r, 500));
+
+      console.log("fini la soumission");
+    },
+  });
+
   return (
     <>
       <h1>Ajouter un article</h1>
-      <form>
-        <label>
-          <span>Nom</span>
-          <input type="text" />
-        </label>
-        <label>
-          <span>Prix</span>
-          <input type="number" />
-        </label>
-        <label>
-          <span>Quantité</span>
-          <input type="number" />
-        </label>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="name"
+          validators={{
+            onChange: ({ value }) =>
+              value.length < 3 ? "Minimum 3 caractères" : undefined,
+          }}
+        >
+          {(field) => (
+            <label>
+              <span>Nom</span>
+              <input
+                type="text"
+                value={field.state.value}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+            </label>
+          )}
+        </form.Field>
+        <form.Field
+          name="price"
+          validators={{
+            onChange: ({ value }) => (value < 0 ? "Prix positif" : undefined),
+          }}
+        >
+          {(field) => (
+            <label>
+              <span>Price</span>
+              <input
+                type="number"
+                value={field.state.value}
+                onChange={(e) => field.handleChange(+e.target.value)}
+                step="0.01"
+              />
+            </label>
+          )}
+        </form.Field>
+        <form.Field
+          name="qty"
+          validators={{
+            onChange: ({ value }) =>
+              value % 1 !== 0 ? "Entier stp" : undefined,
+          }}
+        >
+          {(field) => (
+            <label>
+              <span>Quantité</span>
+              <input
+                type="number"
+                value={field.state.value}
+                onChange={(e) => field.handleChange(+e.target.value)}
+              />
+            </label>
+          )}
+        </form.Field>
+
         <div className="error"></div>
-        <button className="primary">
+        <button className="primary" disabled={!form.state.isValid}>
           <FontAwesomeIcon icon={faPlus} />
           <span>Ajouter</span>
         </button>
